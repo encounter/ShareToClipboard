947f0f4ef88e7e8879553b0459aaeff00a772db7
Luke Street <luke.street@encounterpc.com>
2012-08-10 01:47:17 -0400

    Add backwards compatibility to SDK version 3, fix not exiting on error, extracted string resources

--- src/com/encounterpc/clipboard/ShareActivity.java
+++ src/com/encounterpc/clipboard/ShareActivity.java
@@ -1,10 +1,9 @@
 package com.encounterpc.clipboard;
 
 import android.app.Activity;
-import android.content.ClipData;
-import android.content.ClipboardManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.ClipboardManager;
 import android.widget.Toast;
 
 import static android.widget.Toast.makeText;
@@ -15,9 +14,9 @@ public class ShareActivity extends Activity {
         super.onCreate(savedInstanceState);
         Bundle extras = getIntent().getExtras();
         if (extras != null && extras.getString(android.content.Intent.EXTRA_TEXT) != null) {
-            ((ClipboardManager) getSystemService(CLIPBOARD_SERVICE)).setPrimaryClip(ClipData.newPlainText(extras.getString(Intent.EXTRA_SUBJECT), extras.getString(Intent.EXTRA_TEXT)));
-            makeText(this, "Copied to clipboard!", Toast.LENGTH_LONG).show();
-            finish();
-        } else makeText(this, "No shared content.", Toast.LENGTH_LONG).show();
+            ((ClipboardManager) getSystemService(CLIPBOARD_SERVICE)).setText(extras.getString(Intent.EXTRA_TEXT));
+            makeText(this, getString(R.string.success), Toast.LENGTH_LONG).show();
+        } else makeText(this, getString(R.string.no_content), Toast.LENGTH_LONG).show();
+        finish();
     }
 }
